d8f61c791f6c677b9f709191d8eaf160bb5bcf93
Enrico Carteciano <carteciano.enrico@gmail.com>
2024-02-05 20:46:51 +0100

    test

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   presets: [
     ['next/babel'], // Ensures Next.js features are supported
-    ['@babel/preset-env', { targets: { node: 'current' } }], // Transpile for the current Node.js version
+    ['@babel/preset-env', { targets: { node: 'current' } }], // Transpile for the current Node.js versionn
   ],
 };
 transformIgnorePatterns: ['/node_modules/(?!(node-fetch)/)']
