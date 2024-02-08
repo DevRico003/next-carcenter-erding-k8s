cb8507583a57e767ab3fccefaab4cc357501b5f0
Enrico Carteciano <carteciano.enrico@gmail.com>
2024-02-08 16:41:39 +0100

    feat: added cronjobs

---
+++ k8s/cronjobs/get_request_fetchCarImages.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Die URL, die Sie anrufen möchten
+URL="http://nextjs-service/api/fetchCarImages"
+
+# Durchführen der GET-Anfrage
+curl ${URL}
---
+++ k8s/cronjobs/get_request_mobile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Der Service-Name und Pfad in Kubernetes
+URL="http://nextjs-service/api/mobile"
+
+# Durchführen der GET-Anfrage
+curl ${URL}
