b4335c9f9d3935b9e5997f2c364c406657601b58
DevRico003 <carteciano.enrico@gmail.com>
2024-03-25 10:25:40 +0100

    test

--- src/pages/fahrzeuge.js
+++ src/pages/fahrzeuge.js
@@ -128,7 +128,7 @@ function CarListingLeftSidebar() {
                 <div className="col-lg-12">
                   <div className="show-item-and-filter">
                     <p>
-                      Es gibt <strong>{filteredCars.length}</strong> verfügbare Änderungen
+                      Es gibt <strong>{filteredCars.length}</strong> verfügbare
                       Autos.
                     </p>
                   </div>
